e868acc0bf910cb55e70e46e1a7068011733d43b
unknown <mrzerocrack@gmail.com>
2025-01-03 13:45:40 +0700

    python

--- install - Copy.bat
+++
@@ -1,6 +0,0 @@
-pip install selenium
-pip install webdriver_manager
-pip install undetected_chromedriver
-pip install psutil
-pip install pillow
-pip install requests
---
+++ install/install.bat
@@ -0,0 +1,8 @@
+pip install selenium
+pip install webdriver_manager
+pip install undetected_chromedriver
+pip install psutil
+pip install pillow
+pip install requests
+pip install pandas
+pip install openpyxl
